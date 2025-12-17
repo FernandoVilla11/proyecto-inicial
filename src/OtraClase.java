4fe30073e3dcf846327e27304bdc693396299a50
FernandoVilla11 <jose.villadiego096@gmail.com>
2025-12-16 21:41:54 -0500

    versión base del proyecto

---
+++ src/OtraClase.java
@@ -0,0 +1,16 @@
+class Prueba {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Leyendo instrucción 1");
+        System.out.println("Leyendo instrucción 2");
+    }
+}
+
+public class OtraClase {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Leyendo instrucción 1");
+        System.out.println("Leyendo instrucción 2");
+    }
+}
+    
